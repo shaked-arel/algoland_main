e60b1c56a73b91a0485a8b65a4230a5f54cfa066
Noamls123 <73317511+Noamls123@users.noreply.github.com>
2022-06-22 17:55:53 +0300

    Add files via upload

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -1,3 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+
 import 'SearchSpeed.dart';
 import 'searchVisual.dart';
 import 'SearchProvider.dart';
@@ -9,8 +11,7 @@ import 'package:flutter/material.dart';
 class SearchPage<T extends SearchProvider> extends StatelessWidget {
   final String title;
 
-  SearchPage({required this.title})
-      : assert(title != null);
+  SearchPage({required this.title}) : assert(title != null);
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +20,64 @@ class SearchPage<T extends SearchProvider> extends StatelessWidget {
       key: key,
       children: <Widget>[
         Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 32.0),
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-            const SizedBox(height: 24),
+            // Padding(
+            //   padding: const EdgeInsets.only(top: 0.0),
+            //   child: Text(
+            //     title,
+            //     style: Theme.of(context).textTheme.headline4,
+            //   ),
+            // ),
+            //Image.asset('assets/robot1.png', width: 70),
+            SearchMessage(),
+            //const SizedBox(height: 24),
             //Below can not be constant
+            //const SizedBox(height: 0),
             Expanded(
               child: SearchVisual(),
             ),
-            SearchMessage(),
-            const SizedBox(height: 24),
             SearchSpeed(),
             Search(),
-            const SizedBox(height: 24),
-          ],
-        ),
+
+
+            const SizedBox(height: 70),
+            Stack(children: <Widget>[
+              Align(
+                alignment: Alignment(0.2,0.5),
+                child: Image.asset('assets/board.png', width: 210),
+              ),
+              Align(
+                alignment: Alignment(-0.8, 0.5),
+                child: Image.asset('assets/teach.png', width: 80),
+              ),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                      width: 300,
+                      height: 120,
+                      child:
+                      Align(
+                        //alignment: Alignment.bottomRight,
+                          child: Text(
+                            'Remember! \r\n \r\n'
+                                'The time complexity \r\n'
+                                'is O(log n).',
+                            style: GoogleFonts.robotoFlex(color: Colors.black, fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          )))),
+            ]),
+
+            //const SizedBox(height: 24),
+          ],),
         SearchIndicator(
           parentKey: key,
         ),
+        const SizedBox(height: 70),
+        // Align(
+        //   alignment: Alignment.bottomLeft,
+        //   child: Image.asset('assets/simulations.png', width: 100),
+        // ),
       ],
     );
   }
--- lib/SortPage.dart
+++ lib/SortPage.dart
@@ -1,3 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+
 import 'SortButton.dart';
 import 'SortProvider.dart';
 import 'Speed.dart';
@@ -15,36 +17,70 @@ class SortPage<T extends SortProvider> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (_, constraints) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 100.0),
-              child: Text(
-                title,
-                style: TextStyle(
-                    color: Colors.orange,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: constraints.maxWidth,
-                child: Center(
-                  child: SortVisual(
-                    blockSize: BlockSize,
-                    width: constraints.maxWidth,
+      return Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              )),
+          Align(
+              alignment: Alignment.topCenter,
+              child: Expanded(
+                child: Container(
+                  width: constraints.maxWidth,
+                  child: Center(
+                    child: SortVisual(
+                      blockSize: BlockSize,
+                      width: constraints.maxWidth,
+                    ),
                   ),
                 ),
-              ),
+              )),
+          //Buttons(),
+          SizedBox(
+            height: 10,
+          ),
+          Align(alignment: Alignment.bottomCenter, child: Speed()),
+          Align(alignment: Alignment.bottomCenter, child: SortButton()),
+          //Align(
+          //alignment: Alignme,
+          Stack(children: <Widget>[
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/board.png', width: 250),
             ),
-            Buttons(),
-            Speed(),
-            SortButton(),
-          ],
-        ),
+            Align(
+              alignment: Alignment(-1, 0),
+              child: Image.asset('assets/teach.png', width: 120),
+            ),
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                    width: 300,
+                    height: 120,
+                    child: Align(
+                        //alignment: Alignment.bottomRig,
+                        child: Text(
+                          '   Remember! \r\n \r\n'
+                          '   The time complexity \r\n'
+                              '   is O(n^2).',
+                          style: GoogleFonts.robotoFlex(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        )))),
+          ]),
+        ],
       );
     });
   }
