76ab0da3b0095967f9eb565cc1305a3f4384aa19
Noamls123 <73317511+Noamls123@users.noreply.github.com>
2022-06-22 21:55:03 +0300

    Add files via upload

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,11 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:binarysearch/Algorithms.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import './auth.dart';
 import 'config/palette.dart';
@@ -12,7 +17,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
         splash: Center(
-          child: Lottie.asset('assets/loading.json', fit: BoxFit.cover),
+          child: Image.asset('assets/Hello.gif', height: 200, ),
         ),
         nextScreen: HomePage());
   }
@@ -22,42 +27,132 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         //backgroundColor: Colors.blue,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                new MaterialPageRoute(builder: (context) => AuthGate()));
-          },
-          tooltip: 'Next Page',
-          child: IconTheme(
-            data: new IconThemeData(
-              color: Colors.blue,
-            ),
-            child: new Icon(Icons.arrow_forward),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: Stack(children: <Widget>[
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     Navigator.push(context,
+        //         new MaterialPageRoute(builder: (context) => AuthGate()));
+        //   },
+        //   tooltip: 'Next Page',
+        //   child: IconTheme(
+        //     data: new IconThemeData(
+        //       color: Palette.darkBlue2,
+        //     ),
+        //     child: new Icon(Icons.arrow_forward),
+        //   ),
+        //   backgroundColor: Colors.white,
+        // ),
+        body: Stack(
+            children: <Widget>[
+              // Align(alignment: Alignment.topRight,
+              //     child: FloatingActionButton(
+              //   onPressed: () {
+              //     _signOut();
+              //     Navigator.push(context,
+              //         new MaterialPageRoute(builder: (context) => SignInScreen(
+              //           providerConfigs: [
+              //             EmailProviderConfiguration(),
+              //           ],
+              //         )));
+              //       },
+              //     tooltip: 'Next Page',
+              //     child: IconTheme(
+              //     data: new IconThemeData(
+              //       color: Palette.darkBlue2,
+              //     ),
+              //     child: new Icon(Icons.logout),
+              //   ),
+              //   backgroundColor: Colors.white,
+              // )),
           Center(
               //child: Lottie.asset('assets/logo.json')),
-              child: Image.asset('assets/logo_home.png', width: 250)),
+              child: Image.asset('assets/Hello.gif', width: 250)),
           Align(
-              alignment: const Alignment(0.1, 0.6),
-              child: DefaultTextStyle(
-                style: TextStyle(
-                  fontSize: 30.0,
+              alignment: const Alignment(0, 0.32),
+              child: Text('Algoland',
+                style: GoogleFonts.robotoFlex(
+                  fontSize: 40.0,
                   fontWeight: FontWeight.bold,
-                  color: Palette.lightBlue2,
+                  color: Palette.darkBlue2,
+                ),),),
+    Align(
+      alignment: Alignment(0, 0.65),
+      child: Padding(
+          padding:  const EdgeInsets.all(0),
+          child:  Container(
+              width: 180,
+              height: 40,
+              child: ElevatedButton(
+
+                style: ElevatedButton.styleFrom(
+                  side: BorderSide(width: 2.0, color: Palette.darkBlue2,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),),
+                  primary: Colors.white,
+                  //primary: _hasBeenPressed ? Colors.grey : Palette.lightBlue2,
                 ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    WavyAnimatedText('Algoland'),
-                  ],
-                  isRepeatingAnimation: false,
-                  onTap: () {
-                    print("Tap Event");
-                  },
+                onPressed: () {
+                  Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => AuthGate()));
+                },
+                child: Text(
+                "Get Started",
+                style: GoogleFonts.robotoFlex(color: Colors.black, fontSize: 15,),
+              ),
+              )),
+      )),
+      Align(
+        alignment: Alignment(0, 0.8),
+        child: Padding(
+          padding:  const EdgeInsets.all(0),
+          child:  Container(
+              width: 180,
+              height: 40,
+              child: ElevatedButton(
+
+                style: ElevatedButton.styleFrom(
+                  side: BorderSide(width: 2.0, color: Palette.darkBlue2,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),),
+                  primary: Colors.white,
+                  //primary: _hasBeenPressed ? Colors.grey : Palette.lightBlue2,
                 ),
-              ))
-        ]),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        backgroundColor: Color(0xfbfbfbfb),
+                        title: Text('Algoland', textAlign: TextAlign.center, style: GoogleFonts.robotoFlex(color: Colors.orange, fontWeight: FontWeight.bold),),
+                        content:
+                        Container(
+                            width: 200,
+                            height: 320,
+                            child: Column(
+                              children: <Widget>[
+                                Text( 'Algoland is a friendly app designed to help you learn algorithms in an experiential way. \r\n'
+                                    'You can learn by simulation, quiz and even games. \r\n'
+                                    'Hope you enjoy!', textAlign: TextAlign.center,
+                                  style: GoogleFonts.robotoFlex(fontSize: 21),),
+                                SizedBox(height: 13,),
+                                Align(alignment: Alignment.bottomCenter,
+                                  child: Image.asset(
+                                      "assets/dialog.png"),),
+                              ],
+                            )
+                        ),
+                      ));
+                },
+                child: Text(
+                  "About",
+                  style: GoogleFonts.robotoFlex(color: Colors.black, fontSize: 15),
+                ),
+              )),
+        ),
+    )],),
       );
+}
+
+Future<void> _signOut() async {
+  await FirebaseAuth.instance.signOut();
 }
