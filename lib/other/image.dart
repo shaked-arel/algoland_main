00b55a9cc16af95650da709c2ddeff5b4e0f24e4
Noamls123 <73317511+Noamls123@users.noreply.github.com>
2022-07-06 12:43:21 +0300

    Add files via upload

---
+++ lib/other/image.dart
@@ -0,0 +1,8 @@
+class Images{
+  static const String trueAns = 'assets/true.png';
+  static const String falseAns = 'assets/false.png';
+  static const String question = 'assets/question.png';
+  static const String easyLevel = 'assets/game-backg.png';
+  static const String goodGif = 'assets/good.gif';
+  static const String againGif = 'assets/tryAgain.gif';
+}
